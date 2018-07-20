692cbee2a2b8c398fa449f4a247c5d649afac5e3
Genbu Hase <hasegenbu@gmail.com>
2018-07-20 17:01:19 +0900

    使用例製作完了

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
-const loader = new TemplateLoader("assets/template");
-loader.on("load").then(() => console.log(loader.template));
+/** @type {HTMLInputElement} */
+const nameInputter = document.getElementById("builder-name");
+/** @type {HTMLTextAreaElement} */
+const bioInputter = document.getElementById("builder-bio");
+/** @type {HTMLButtonElement} */
+const buildBtn = document.getElementById("builder-build");
+/** @type {HTMLElement} */
+const cardViewer = document.getElementById("viewer");
+/** @type {HTMLTextAreaElement} */
+const codeInputter = document.getElementById("viewer-codeViewer-codes");
+
+
+
+const loader = new TemplateLoader("assets/template/");
+loader.on("load").then(() => {
+	console.log(loader.template);
+
+	buildBtn.addEventListener("click", () => {
+		for (const child of cardViewer.children) child.classList.contains("AuthorCard") && cardViewer.removeChild(child);
+
+		const card = loader.createComponent("AuthorCard", nameInputter.value || "​", bioInputter.value || "​");
+		cardViewer.insertBefore(card, cardViewer.lastElementChild);
+
+		codeInputter.value = card.outerHTML;
+		M.textareaAutoResize(codeInputter);
+	});
+});
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+	codeInputter.addEventListener("click", () => {
+		codeInputter.select();
+	});
+});
